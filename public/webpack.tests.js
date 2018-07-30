2ed38957439d0c7e24293c5160c768127652c1de
Sergio Mazzoleni <sergio.mazzoleni@gmail.com>
2018-07-30 02:28:17 +0200

    incude .spec.ts files as well

--- public/webpack.tests.js
+++ public/webpack.tests.js
@@ -1,4 +1,4 @@
 import './src/app';
 
-let testsContext = require.context('./src', true, /\.spec$/);
+let testsContext = require.context('./src', true, /\.spec\.[jt]s$/);
 testsContext.keys().forEach(testsContext);
