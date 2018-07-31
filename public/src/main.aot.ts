42e13b0f8f0aa6aeab3dc891a6fb6ebb87b61442
Sergio Mazzoleni <sergio.mazzoleni@gmail.com>
2018-08-01 01:35:20 +0200

    remove setAngularLib in main and main.aot

--- public/src/main.aot.ts
+++ public/src/main.aot.ts
@@ -1,13 +1,8 @@
 import 'zone.js';
 import 'reflect-metadata';
 import { platformBrowser } from '@angular/platform-browser';
-import { setAngularLib } from '@angular/upgrade/static';
 import { AppModuleNgFactory } from './app.module.ngfactory';
 import { enableProdMode } from '@angular/core';
 
-import * as angular from 'angular';
-import { AppModule } from './app.module';
-
-setAngularLib(angular);
 enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
